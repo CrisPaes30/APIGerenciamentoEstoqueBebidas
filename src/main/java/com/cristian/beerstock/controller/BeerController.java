f4e66edd3b25cb3de7b0a5c6147527b6402f584d
CrisPAes30 <cristianpaes90@gmail.com>
2021-02-15 12:30:10 -0300

    Teste unitario

---
+++ src/main/java/com/cristian/beerstock/controller/BeerController.java
@@ -0,0 +1,53 @@
+package com.cristian.beerstock.controller;
+
+import com.cristian.beerstock.dto.BeerDTO;
+import com.cristian.beerstock.dto.QuantityDTO;
+import com.cristian.beerstock.exception.BeerAlreadyRegisteredException;
+import com.cristian.beerstock.exception.BeerNotFoundException;
+import com.cristian.beerstock.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+}
---
+++ src/main/java/com/cristian/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.cristian.beerstock.mapper;
+
+import com.cristian.beerstock.dto.BeerDTO;
+import com.cristian.beerstock.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
